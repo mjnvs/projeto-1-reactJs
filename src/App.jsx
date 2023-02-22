b660ebc203ce8611ef7f36cf9ab68e2f94bca7d1
Marcelo Neves <mjnvs@uol.com.br>
2023-02-22 07:33:04 -0300

    Css Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+import './styles.css'
 
 export function App() {
   
   return (
     <div>
-      <Post/>
+      <Header/>
+      <Post
+        author="Marcelo Neves"
+        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum dolorum soluta omnis fuga vero ipsum iusto magni. Eum modi molestiae optio ducimus dolores, sequi a explicabo voluptatibus enim quas odit." 
+      />
     </div>
   )
 }
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,3 +1,9 @@
-export function Post(){
-    return <p>Post</p>
+export function Post(props){
+    return (
+        <div>
+            <strong><p>{props.author}</p></strong>
+            <p>{props.content}</p>
+        </div>
+    )    
 }
+
